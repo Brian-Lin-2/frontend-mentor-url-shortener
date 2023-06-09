e542964a758e4aac65bb1fcb6c9a20af5ca70e99
Brian <linbrian24@gmail.com>
2023-06-08 22:07:59 -0400

    Import google font, colors, and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: "hsl(180, 66%, 49%)",
+        "dark-violet": "hsl(257, 27%, 26%)",
+        red: "hsl(0, 87%, 67%)",
+        gray: "hsl(0, 0%, 75%)",
+        "grayish-violet": "hsl(257, 7%, 63%)",
+        "v-dark-blue": "hsl(255, 11%, 22%)",
+        "v-dark-violet": "hsl(260, 8%, 14%)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      width: {
+        mobile: "375px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
