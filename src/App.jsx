d032342e711170174c3f108b48ac576eafaa3d4b
Brian <linbrian24@gmail.com>
2023-06-08 21:45:15 -0400

    Create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
-export default function App() {}
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
