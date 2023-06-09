6289fcc824f04274095036e0f03aa4758205f4a6
Brian <linbrian24@gmail.com>
2023-06-09 16:54:00 -0400

    Style Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,21 @@
 export default function Header() {
   return (
-    <div>
-      <div>
+    <div className="p-4 mb-20">
+      <div className="flex justify-between mb-12">
         <img src="./images/logo.svg" alt="shortly" />
         <img className="w-8" src="./images/icon-hamburger.svg" alt="" />
       </div>
 
       <img src="./images/illustration-working.svg" alt="hero" />
-      <div>
-        <h1>More than just shorter links</h1>
-        <p>
+      <div className="mt-6 flex flex-col items-center text-center">
+        <h1 className="text-4xl font-bold">More than just shorter links</h1>
+        <p className="text-grayish-violet mx-6 mt-3 mb-6">
           Build your brand’s recognition and get detailed insights on how your
           links are performing.
         </p>
-        <button>Get Started</button>
+        <button className="text-lg bg-cyan text-white py-2 px-10 rounded-3xl">
+          Get Started
+        </button>
       </div>
     </div>
   );
