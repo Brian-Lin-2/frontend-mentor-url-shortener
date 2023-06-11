253bef1612626a74df23eb41dd2546aca69af3c4
Brian <linbrian24@gmail.com>
2023-06-11 11:55:21 -0400

    Fix mobile active state

--- src/components/body/Shorten.jsx
+++ src/components/body/Shorten.jsx
@@ -1,18 +1,23 @@
 export default function Shorten() {
   return (
     <>
-      <form className="bg-dark-violet bg-shorten-pattern-mob bg-right-top bg-no-repeat mx-8 pt-6 px-2 rounded-lg relative bottom-20 lg:bg-shorten-pattern-desk lg:mx-48 lg:pt-12 lg:pb-6 lg:px-14 lg:bottom-18">
+      <form className="bg-dark-violet bg-shorten-pattern-mob bg-right-top bg-no-repeat mx-8 py-6 px-2 rounded-lg relative bottom-20 lg:bg-shorten-pattern-desk lg:mx-48 lg:pt-12 lg:pb-6 lg:px-14 lg:bottom-18">
         <div className="flex flex-col items-center lg:flex-row lg:gap-6">
           <input
-            className="outline-none w-11/12 mb-4 rounded-md p-4 pb-3 text-sm outline-2 -outline-offset-1 outline-red lg:mb-0 lg:w-4/5 lg:text-lg lg:pl-8"
+            className="outline-none w-11/12 rounded-md p-4 pb-3 text-sm outline-2 -outline-offset-1 outline-red lg:mb-0 lg:w-4/5 lg:text-lg lg:pl-8"
             type="text"
             placeholder="Shorten a link here..."
           ></input>
-          <button className="text-white bg-cyan p-3 w-11/12 rounded-md lg:w-1/5 lg:text-lg lg:py-3.5 hover:bg-light-cyan">
+          <p className="text-red mt-2 italic text-sm ml-16 w-full lg:hidden">
+            Please add a link
+          </p>
+          <button className="text-white bg-cyan mt-4 p-3 w-11/12 rounded-md lg:w-1/5 lg:text-lg lg:py-3.5 lg:mt-0 hover:bg-light-cyan">
             Shorten it!
           </button>
         </div>
-        <p className="text-red mt-2 italic text-sm ml-8">Please add a link</p>
+        <p className="hidden text-red mt-2 italic text-sm ml-8 lg:block">
+          Please add a link
+        </p>
       </form>
     </>
   );
