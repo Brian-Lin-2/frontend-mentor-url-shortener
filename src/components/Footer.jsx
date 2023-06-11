e14b77643691f242d27d9a55425b05d82f6ebd8c
Brian <linbrian24@gmail.com>
2023-06-11 00:04:27 -0400

    Style Footer (desktop)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
-    <div className="text-white bg-v-dark-violet flex flex-col gap-12 p-12 items-center text-center">
-      <h1 className="text-4xl">Shortly</h1>
+    <div className="text-white bg-v-dark-violet flex flex-col gap-12 p-12 items-center text-center lg:flex-row lg:items-start lg:text-start lg:justify-evenly lg:gap-0 lg:p-16">
+      <h1 className="text-4xl lg:mr-40">Shortly</h1>
       <div>
-        <h3 className="mb-6">Features</h3>
+        <h3 className="mb-6 lg:mt-2">Features</h3>
         <div className="text-grayish-violet flex flex-col gap-3">
           <a>Link Shortening</a>
           <a>Branded Links</a>
@@ -12,7 +12,7 @@ export default function Footer() {
       </div>
 
       <div>
-        <h3 className="mb-6">Resources</h3>
+        <h3 className="mb-6 lg:mt-2">Resources</h3>
         <div className="text-grayish-violet flex flex-col gap-3">
           <a>Blog</a>
           <a>Developers</a>
@@ -21,7 +21,7 @@ export default function Footer() {
       </div>
 
       <div>
-        <h3 className="mb-6">Company</h3>
+        <h3 className="mb-6 lg:mt-2">Company</h3>
         <div className="text-grayish-violet flex flex-col gap-3">
           <a>About</a>
           <a>Our Team</a>
@@ -30,7 +30,7 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className="flex gap-6">
+      <div className="flex gap-6 lg:ml-6 lg:mt-2">
         <img src="./images/icon-facebook.svg" alt="" />
         <img src="./images/icon-twitter.svg" alt="" />
         <img src="./images/icon-pinterest.svg" alt="" />
