cb8d916d89b34660e5c76023aea2435803e4890a
Brian <linbrian24@gmail.com>
2023-06-12 22:27:56 -0400

    Add toggle state and active state to Mobile Menu

--- src/components/header/MobileMenu.jsx
+++ src/components/header/MobileMenu.jsx
@@ -1,19 +1,25 @@
-export default function MobileMenu() {
+export default function MobileMenu({ setOpenMenu }) {
   return (
     <div className="fixed inset-0 bg-v-dark-blue text-white flex flex-col items-center gap-8 text-xl z-20">
-      <img
-        className="absolute right-0 p-6 w-20"
-        src="./images/icon-close.svg"
-        alt=""
-      />
-      <div className="mt-32 border-b flex flex-col items-center gap-6 pb-8 px-28">
-        <a>Features</a>
-        <a>Pricing</a>
-        <a>Resources</a>
+      <a>
+        <img
+          className="absolute right-0 p-6 w-20 hover:cursor-pointer"
+          src="./images/icon-close.svg"
+          alt=""
+          onClick={() => setOpenMenu(false)}
+        />
+      </a>
+
+      <div className="mt-32 border-b flex flex-col items-center gap-8 pb-8 px-28">
+        <a className="hover:text-cyan hover:cursor-pointer">Features</a>
+        <a className="hover:text-cyan hover:cursor-pointer">Pricing</a>
+        <a className="hover:text-cyan hover:cursor-pointer">Resources</a>
       </div>
 
-      <button>Login</button>
-      <button className="py-2 px-28 bg-cyan rounded-full">Sign Up</button>
+      <button className="hover:text-cyan hover:cursor-pointer">Login</button>
+      <button className="py-2 px-28 bg-cyan rounded-full hover:bg-light-cyan">
+        Sign Up
+      </button>
     </div>
   );
 }
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,16 +1,24 @@
 import MobileMenu from "./MobileMenu";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <div className="lg:flex lg:justify-between lg:items-center lg:p-10">
       <div className="flex justify-between mb-12 lg:justify-start lg:items-center lg:ml-32 lg:mb-0 lg:gap-10">
         <img src="./images/logo.svg" alt="shortly" />
-        <img
-          className="w-8 hover:cursor-pointer lg:hidden"
-          src="./images/icon-hamburger.svg"
-          alt=""
-        />
-        <MobileMenu />
+
+        <a>
+          <img
+            className="w-8 hover:cursor-pointer lg:hidden"
+            src="./images/icon-hamburger.svg"
+            alt=""
+            onClick={() => setOpenMenu(true)}
+          />
+        </a>
+
+        {openMenu && <MobileMenu setOpenMenu={setOpenMenu} />}
 
         <a className="hidden lg:block text-grayish-violet font-bold text-sm hover:text-black hover:cursor-pointer">
           Features
