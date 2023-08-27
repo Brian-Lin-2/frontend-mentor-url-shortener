da11e50a7c3abe6ff8cff2b86d3d73a44a615bb5
Brian <linbrian24@gmail.com>
2023-08-26 22:14:53 -0400

    Add email/password verification for Login

--- src/components/account/Login.jsx
+++ src/components/account/Login.jsx
@@ -1,9 +1,54 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState({ email: false, password: false });
+
+  const validateInfo = () => {
+    // eslint-disable-next-line no-useless-escape
+    const emailRegex = /.+\@.+\..+/;
+
+    if (!emailRegex.test(email) && password === "") {
+      setError({ email: true, password: true });
+      return false;
+    } else if (!emailRegex.test(email)) {
+      setError({ ...error, email: true });
+      return false;
+    } else if (password === "") {
+      setError({ ...error, password: true });
+      return false;
+    } else if (emailRegex.test(email) && password !== "") {
+      setError({ email: false, password: false });
+      return true;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    if (validateInfo()) {
+      // Add backend.
+    } else {
+      e.preventDefault();
+    }
+  };
+
+  let emailStyle = "border-b outline-none";
+  let passwordStyle = "border-b outline-none";
+
+  if (error.email) {
+    emailStyle = "border-b-2 outline-none border-red";
+  }
+  if (error.password) {
+    passwordStyle = "border-b-2 outline-none border-red";
+  }
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-light-gray font-poppins">
-      <form className="bg-white p-10 rounded-lg flex flex-col gap-7 w-72">
+      <form
+        className="bg-white p-10 rounded-lg flex flex-col gap-7 w-72"
+        onSubmit={handleSubmit}
+      >
         <div className="flex items-center gap-11">
           <Link to="/">
             <img className="w-7" src="/images/arrow-back.svg" alt="" />
@@ -11,18 +56,24 @@ export default function Login() {
           <h1 className="text-2xl">Login</h1>
         </div>
         <input
-          className="border-b outline-none"
+          className={emailStyle}
           type="email"
           name="email"
           placeholder="Email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
         <input
-          className="border-b outline-none"
+          className={passwordStyle}
           type="password"
           name="password"
           placeholder="Password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
         />
-        <button className="bg-cyan p-1 rounded text-white hover:text-black">
+        <button className="bg-cyan p-1 rounded text-white hover:opacity-80">
           Login
         </button>
         <Link
@@ -31,6 +82,11 @@ export default function Login() {
         >
           Create Account
         </Link>
+        {(error.email || email.password) && (
+          <p className="text-center -mt-4 -mb-4 text-red">
+            Invalid Information.
+          </p>
+        )}
       </form>
     </div>
   );
